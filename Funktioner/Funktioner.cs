72e0af2217efc66c18f3e3efe073817a10be4be6
Amanda Westerlind <amanda.westerlind@live.se>
2025-09-08 15:49:48 +0200

    Added Funktioner exercise 5 and some of 6.

--- Funktioner/Funktioner.cs
+++ Funktioner/Funktioner.cs
@@ -4,6 +4,8 @@
 //
 
 using System;
+using System.Diagnostics.Tracing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 static void PrintName(string firstName, string lastName)
 {
@@ -56,8 +58,65 @@ double farenheit = (CelsiusToFahrenheit(celsius));
 
 Console.WriteLine($"{celsius}°C blir {farenheit}°F");
 
+
 // Uppgift 5 - Lägg in bindesträck mellan tecken i en sträng
 
+Console.WriteLine();
+static string AddHyphen(string word)
+{
+    char[] addedHyphen = new char[word.Length * 2 - 1];
+
+    for (int i = 0; i < (addedHyphen.Length); i++)
+    {
+        if (i % 2 == 0)
+        {
+            addedHyphen[i] = word[i / 2];
+        }
+        else
+        {
+
+            addedHyphen[i] = '-';
+        }
+
+    }
+
+    string retur = new string(addedHyphen);
+
+    return retur;
+}
+
+string addedHyphen = (AddHyphen("Amanda"));
+Console.WriteLine(addedHyphen);
 
 
 // Uppgift 6 - Egen version av String.Join();
+
+Console.WriteLine();
+static string MyJoin(params string[] words)
+{
+    string[] result = new string[(words.Length+1)];
+
+    for (int i = 0; i < (result.Length); i++)
+    {
+        if (i % 2 == 0)
+        {
+            result[i] = words[0];
+        }
+        else
+        {
+            result[i] = words[i+1];
+        }
+
+    }
+
+    var results = string.Empty;
+    foreach (var item in result)
+    {
+        results += item;
+    }
+
+    return results;
+}
+
+string joinedText = (MyJoin("->", "Göteborg", "Sundsvall", "Stockholm"));
+Console.WriteLine(joinedText);
