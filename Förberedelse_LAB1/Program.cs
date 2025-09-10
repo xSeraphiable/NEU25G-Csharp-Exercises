58d7737a978b66a7b05e2e3da493d743618a30a9
Amanda Westerlind <amanda.westerlind@live.se>
2025-09-10 22:03:26 +0200

    Added new project "Förberedelser LAB1". Solution to exercise 1-4

---
+++ Förberedelse_LAB1/Program.cs
@@ -0,0 +1,81 @@
+﻿
+// UPPGIFT 1 - Bokstav för bokstav - fram till space
+
+using System;
+
+string helloWorld = "Hello World!";
+
+for (int i = 0; i < helloWorld.Length; i++)
+{
+    if (helloWorld[i] == ' ')
+    {
+        break;
+    }
+
+    Console.Write(helloWorld[i]);
+
+}
+Console.WriteLine();
+
+// UPPGIFT 2 - Bokstav för bokstav - radbyte på space
+
+
+string lotsOfWords = "Det här är bara en massa ord utan någon mening.";
+
+for (int i = 0; i < lotsOfWords.Length; i++)
+{
+    if (lotsOfWords[i] == ' ')
+    {
+        Console.WriteLine();
+        continue;
+    }
+
+    Console.Write(lotsOfWords[i]);
+
+}
+
+Console.WriteLine();
+// UPPGIFT 3 - varannan stjärna
+
+string uppgift3 = "Detta är uppgift 3.";
+
+for (int i = 0; i < uppgift3.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(uppgift3[i]);
+    }
+    else
+    {
+      
+         Console.Write("*");
+       
+    }
+
+}
+
+// UPPGIFT 4 - gröna o, röda l
+
+
+
+for (int i = 0; i < helloWorld.Length; i++)
+{
+    if (helloWorld[i] == 'o')
+    {
+       Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(helloWorld[i]);
+        Console.ResetColor();
+    }
+    else if (helloWorld[i] == 'l')
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(helloWorld[i]);
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.Write(helloWorld[i]);
+    }
+
+}
+Console.WriteLine();
